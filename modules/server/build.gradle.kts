ba8f7d9f36f7321c5839e67e6c993726b09b50cf
wezzen <dimariqqq@gmail.com>
2023-07-26 11:40:05 +0600

    build: удалил зависимость от groovy в gradle скрипте

--- modules/server/build.gradle.kts
+++ modules/server/build.gradle.kts
@@ -5,8 +5,6 @@ plugins {
 
 dependencies {
 
-    implementation("org.apache.groovy:groovy:4.0.12")
-
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
