a6f5db4f192373a8569bd3d953f98e3372fe101e
wezzen <dimariqqq@gmail.com>
2023-07-27 19:03:55 +0600

    chore: Вынес общие зависимости gradle в project build.gradle.kts, добавил jacoco

--- modules/client/build.gradle.kts
+++ modules/client/build.gradle.kts
@@ -4,11 +4,4 @@ plugins {
 }
 
 dependencies {
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testImplementation("junit:junit:4.13.1")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
