06ef81771fa4735e696bc939c7b13fde44ecb424
wezzen <dimariqqq@gmail.com>
2023-07-27 19:21:50 +0600

    test: Добавил тесты на создание экземпляра для покрытия в 100%

--- modules/server/src/test/java/io/deeplay/intership/server/ServerTest.java
+++ modules/server/src/test/java/io/deeplay/intership/server/ServerTest.java
@@ -8,6 +8,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class ServerTest {
 
     @Test
+    void creatingInstanceTest() {
+        assertDoesNotThrow(Server::new);
+    }
+
+    @Test
     void mainThrowTest() {
         assertDoesNotThrow(() -> Server.main(new String[0]));
     }
