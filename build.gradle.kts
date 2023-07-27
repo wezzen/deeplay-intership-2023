4943479d58b79134ed327e213373d4a7bf8ae117
wezzen <dimariqqq@gmail.com>
2023-07-27 19:03:55 +0600

    chore: Вынес общие зависимости gradle в project build.gradle.kts, добавил jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,42 @@
 plugins {
+    id("java")
     id("net.ltgt.errorprone") version "3.1.0"
+    id("jacoco")
 }
 
 allprojects {
     apply(plugin = "net.ltgt.errorprone")
+    apply(plugin = "jacoco")
+    apply(plugin = "java")
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     dependencies {
         errorprone("com.google.errorprone:error_prone_core:2.20.0")
+
+        testImplementation(platform("org.junit:junit-bom:5.9.3"))
+        testImplementation("org.junit.jupiter:junit-jupiter")
     }
 
     repositories {
         mavenCentral()
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+        finalizedBy(tasks.jacocoTestReport)
+    }
+
+    tasks.jacocoTestReport {
+        dependsOn(tasks.test)
+        reports {
+            xml.required.set(true)
+            csv.required.set(false)
+            html.required.set(false)
+        }
     }
 }
 
