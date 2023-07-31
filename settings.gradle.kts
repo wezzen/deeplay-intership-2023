91bd97b1dd0ef575377002975deb3b171ad2f957
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-08-01 01:05:55 +0600

    добавлена конфигурация модуля для gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,3 +5,6 @@ project(":client").projectDir = file("modules/client")
 
 include("server")
 project(":server").projectDir = file("modules/server")
+
+include("game-service")
+project(":game-service").projectDir = file("modules/game-service")
