aac4a52e2206c6656dbefa0e295b0c192fdb9bf7
Simon170522 <safonof.com@gmail.com>
2023-07-31 16:47:51 +0700

    Изменил Javadoc. Добавил методы

--- modules/server/src/main/java/io/deeplay/inership/server/Board.java
+++ modules/server/src/main/java/io/deeplay/inership/server/Board.java
@@ -1,12 +1,51 @@
 
 package io.deeplay.inership.server;
-
 import java.util.Set;
 
+/**
+ * Класс описывающий доску Гобан размером 9х9
+ */
 public class Board {
+    /**
+     * Ширина
+     */
     private final int WEIGHT = 9;
+
+    /** Высота */
     private final int HEIGHT = 9;
+    /** Поле игры*/
     private Stone[][] field;
+
+    /** Множество групп камней */
     private Set<Group> stoneGroups;
 
+    /**
+     * Установить камень на позицию
+     *
+     * @param xPosition  (1..9)
+     * @param yPosition  (1..9)
+     * @param stoneColor Цвет камня
+     * @return true если поставил камень
+     */
+    public boolean setStone(int xPosition, int yPosition, Color stoneColor) {
+        boolean answer;
+        if (isEmptyCell(xPosition, yPosition)) {
+            answer = true;
+            field[xPosition][yPosition].setColor(stoneColor);
+        } else {
+            answer = false;
+        }
+        return answer;
+    }
+
+    /**
+     * Проверить свободное место на позиции
+     *
+     * @param xPosition (1..9)
+     * @param yPosition (1..9)
+     * @return true если не вышли за границы и место свободно
+     */
+    private boolean isEmptyCell(int xPosition, int yPosition) {
+        return (xPosition <= HEIGHT && xPosition >= 1) && (yPosition < WEIGHT && xPosition >= 1) && (field[xPosition][yPosition].getColor() == Color.EMPTY);
+    }
 }
--- modules/server/src/main/java/io/deeplay/inership/server/Stone.java
+++ modules/server/src/main/java/io/deeplay/inership/server/Stone.java
@@ -5,4 +5,12 @@ public class Stone {
     private Color color;
     private Cell cell;
     private Group sequence;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color1) {
+        this.color = color1;
+    }
 }
