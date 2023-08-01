cb6bd6c966e658777710609a19961d59c3b9393c
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-08-01 10:21:34 +0600

    Добавил тест для второго конструктора класса Stone

--- modules/game-service/src/test/java/io/deeplay/intership/service/StoneTest.java
+++ modules/game-service/src/test/java/io/deeplay/intership/service/StoneTest.java
@@ -3,6 +3,7 @@ package io.deeplay.intership.service;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -26,7 +27,22 @@ public class StoneTest {
         () -> assertNotNull(stone.getGroup()),
         () -> assertEquals(group, stone.getGroup())
     );
+  }
 
+  @Test
+  public void testStoneConstructor() {
+    Cell cell = Mockito.mock(Cell.class);
+    Color color = Mockito.mock(Color.class);
+    Stone stone = new Stone(color, cell);
 
+    assertAll(
+        () -> assertNotNull(stone.getColor()),
+        () -> assertEquals(color, stone.getColor()),
+
+        () -> assertNotNull(stone.getCell()),
+        () -> assertEquals(cell, stone.getCell()),
+
+        () -> assertNull(stone.getGroup())
+    );
   }
 }
