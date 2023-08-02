00eb485026ec1aa7a49b2b8e10a6d7ea5632d803
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-08-02 21:55:14 +0600

    test: добавил еще тесты

--- modules/game-service/src/test/java/io/deeplay/intership/service/StoneTest.java
+++ modules/game-service/src/test/java/io/deeplay/intership/service/StoneTest.java
@@ -1,50 +1,53 @@
 package io.deeplay.intership.service;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.Mockito.mock;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 public class StoneTest {
 
-  @Test
-  public void testStone() {
-    int x = 1;
-    int y = 1;
-    Color color = mock(Color.class);
-    Group group = mock(Group.class);
-    Stone stone = new Stone(color, x, y, group);
+    @Test
+    public void testStone() {
+        int x = 1;
+        int y = 1;
+        Color color = mock(Color.class);
+        Group group = mock(Group.class);
+        Stone stone = new Stone(color, x, y, group);
 
-    assertAll(
-        () -> assertNotNull(stone.getColor()),
-        () -> assertEquals(color, stone.getColor()),
+        assertAll(
+                () -> assertNotNull(stone.getColor()),
+                () -> assertEquals(color, stone.getColor()),
 
-        () -> assertEquals(x, stone.getRowNumber()),
-        () -> assertEquals(y, stone.getColumnNumber()),
+                () -> assertEquals(x, stone.getRowNumber()),
+                () -> assertEquals(y, stone.getColumnNumber()),
 
-        () -> assertNotNull(stone.getGroup()),
-        () -> assertEquals(group, stone.getGroup())
-    );
-  }
+                () -> assertNotNull(stone.getGroup()),
+                () -> assertEquals(group, stone.getGroup())
+        );
+    }
 
-  @Test
-  public void testStoneConstructor() {
-    int x = 1;
-    int y = 1;
-    Color color = mock(Color.class);
-    Stone stone = new Stone(color, x, y);
+    @Test
+    public void testStoneConstructor() {
+        int x = 1;
+        int y = 1;
+        Color color = mock(Color.class);
+        Stone stone = new Stone(color, x, y);
 
-    assertAll(
-        () -> assertNotNull(stone.getColor()),
-        () -> assertEquals(color, stone.getColor()),
+        assertAll(
+                () -> assertNotNull(stone.getColor()),
+                () -> assertEquals(color, stone.getColor()),
 
-        () -> assertEquals(x, stone.getRowNumber()),
-        () -> assertEquals(y, stone.getColumnNumber()),
+                () -> assertEquals(x, stone.getRowNumber()),
+                () -> assertEquals(y, stone.getColumnNumber()),
 
-        () -> assertNull(stone.getGroup())
-    );
-  }
+                () -> assertNull(stone.getGroup())
+        );
+    }
+
+    @Test
+    public void testEquals() {
+        Stone stone1 = new Stone(Color.EMPTY, 1, 1);
+        assertNotEquals(stone1, null);
+    }
 }
