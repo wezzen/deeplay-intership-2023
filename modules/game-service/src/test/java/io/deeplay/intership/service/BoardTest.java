bd61c0d08096c62f08978f1a0dddd10676296d3f
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-08-02 21:58:36 +0600

    refactor: отформатировал код по дефолтному стандарту

--- modules/game-service/src/test/java/io/deeplay/intership/service/BoardTest.java
+++ modules/game-service/src/test/java/io/deeplay/intership/service/BoardTest.java
@@ -1,34 +1,34 @@
 package io.deeplay.intership.service;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.junit.jupiter.api.Test;
-
 public class BoardTest {
 
-  @Test
-  public void testBoardInitField() {
-    Board board = new Board();
-    board.getField();
+    @Test
+    public void testBoardInitField() {
+        Board board = new Board();
+        board.getField();
 
-    assertNotNull(board.getField());
-    for (int i = 0; i < board.getField().length; i++) {
-      for (int j = 0; j < board.getField()[i].length; j++) {
-        assertEquals(Color.EMPTY, board.getField()[i][j].getColor());
-        assertEquals(i, board.getField()[i][j].getRowNumber());
-        assertEquals(j, board.getField()[i][j].getColumnNumber());
-      }
+        assertNotNull(board.getField());
+        for (int i = 0; i < board.getField().length; i++) {
+            for (int j = 0; j < board.getField()[i].length; j++) {
+                assertEquals(Color.EMPTY, board.getField()[i][j].getColor());
+                assertEquals(i, board.getField()[i][j].getRowNumber());
+                assertEquals(j, board.getField()[i][j].getColumnNumber());
+            }
+        }
     }
-  }
 
-  @Test
-  public void testBoardInitGroups() {
-    Board board = new Board();
-    board.getField();
+    @Test
+    public void testBoardInitGroups() {
+        Board board = new Board();
+        board.getField();
 
-    assertNotNull(board.getGroups());
-    assertNotNull(board.getField());
-    assertEquals(0, board.getGroups().size());
-  }
+        assertNotNull(board.getGroups());
+        assertNotNull(board.getField());
+        assertEquals(0, board.getGroups().size());
+    }
 }
--- modules/terminal-ui/src/main/java/io/deeplay/intership/ui/terminal/InputUtil.java
+++ modules/terminal-ui/src/main/java/io/deeplay/intership/ui/terminal/InputUtil.java
@@ -2,6 +2,7 @@ package io.deeplay.intership.ui.terminal;
 
 import io.deeplay.intership.service.Color;
 import io.deeplay.intership.service.Stone;
+
 import java.util.Scanner;
 
 /**
@@ -10,55 +11,55 @@ import java.util.Scanner;
  */
 public class InputUtil {
 
-  private final Scanner scanner;
-  private final Converter converter;
-  private final Validator validator;
+    private final Scanner scanner;
+    private final Converter converter;
+    private final Validator validator;
 
-  /**
-   * Создает новый объект InputUtil с {@link Converter} по умолчанию.
-   */
-  public InputUtil() {
-    this.scanner = new Scanner(System.in);
-    this.converter = new Converter();
-    this.validator = new Validator();
-  }
+    /**
+     * Создает новый объект InputUtil с {@link Converter} по умолчанию.
+     */
+    public InputUtil() {
+        this.scanner = new Scanner(System.in);
+        this.converter = new Converter();
+        this.validator = new Validator();
+    }
 
-  /**
-   * Читает пользовательский ввод и преобразует его в объект {@link Stone}.
-   *
-   * @return созданный объект {@link Stone} с координатами на основе пользовательского ввода
-   * @throws IllegalArgumentException, если ввод не в правильном формате
-   */
-  public Stone inputMove(Color color) {
-    String input = scanner.nextLine();
-    return converter.convertStringToStone(input, color);
-  }
+    /**
+     * Читает пользовательский ввод и преобразует его в объект {@link Stone}.
+     *
+     * @return созданный объект {@link Stone} с координатами на основе пользовательского ввода
+     * @throws IllegalArgumentException, если ввод не в правильном формате
+     */
+    public Stone inputMove(Color color) {
+        String input = scanner.nextLine();
+        return converter.convertStringToStone(input, color);
+    }
 
-  /**
-   * Читает пользовательский ввод и преобразует его в объект {@link UserAction}.
-   *
-   * @return объект {@link UserAction} на основе пользовательского ввода
-   */
-  public UserAction inputAction() {
-    String input;
-    do {
-      input = scanner.nextLine();
-    } while (validator.isValidAction(input));
+    /**
+     * Читает пользовательский ввод и преобразует его в объект {@link UserAction}.
+     *
+     * @return объект {@link UserAction} на основе пользовательского ввода
+     */
+    public UserAction inputAction() {
+        String input;
+        do {
+            input = scanner.nextLine();
+        } while (validator.isValidAction(input));
 
-    return converter.convertStringToAction(input);
-  }
+        return converter.convertStringToAction(input);
+    }
 
-  /**
-   * Читает пользовательский ввод и преобразует его в объект {@link UserAction}, пока пользователь
-   * не введет действие, отвечающее за выбор цвета.
-   *
-   * @return объект {@link UserAction} на основе пользовательского ввода
-   */
-  public UserAction inputColorAction() {
-    UserAction action;
-    do {
-      action = inputAction();
-    } while (validator.isColorAction(action));
-    return action;
-  }
+    /**
+     * Читает пользовательский ввод и преобразует его в объект {@link UserAction}, пока пользователь
+     * не введет действие, отвечающее за выбор цвета.
+     *
+     * @return объект {@link UserAction} на основе пользовательского ввода
+     */
+    public UserAction inputColorAction() {
+        UserAction action;
+        do {
+            action = inputAction();
+        } while (validator.isColorAction(action));
+        return action;
+    }
 }
