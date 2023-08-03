d4acc4c11dd8b78658b98c47c9bdb75f22560c14
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-08-04 00:29:14 +0600

    feat: добавил класс, проверяющий состояние игры на возможность завершения

---
+++ modules/game/src/test/java/io/deeplay/intership/service/CheckGameOverTest.java
@@ -0,0 +1,66 @@
+package io.deeplay.intership.service;
+
+import io.deeplay.intership.game.CheckGameOver;
+import io.deeplay.intership.game.Color;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CheckGameOverTest {
+    private static final int STONE_COUNT = 160;
+
+    @Test
+    public void testMaxCanMakeMove() {
+        CheckGameOver checkGameOver = new CheckGameOver();
+        Color color = Color.BLACK;
+
+        for (int i = 0; i < STONE_COUNT; i++) {
+            assertTrue(checkGameOver.canMakeMove(color));
+        }
+    }
+
+    @Test
+    public void testCanMakeMove_ForDifferentColors() {
+        CheckGameOver checkGameOver = new CheckGameOver();
+        assertAll(
+                () -> assertTrue(checkGameOver.canMakeMove(Color.BLACK)),
+                () -> assertTrue(checkGameOver.canMakeMove(Color.WHITE)),
+                () -> assertThrows(IllegalArgumentException.class, () -> checkGameOver.canMakeMove(Color.EMPTY))
+        );
+    }
+
+    @Test
+    public void testCanMakeMove_ForTwoStones() {
+        CheckGameOver checkGameOver = new CheckGameOver();
+        for (int i = 0; i < STONE_COUNT; i++) {
+            assertTrue(checkGameOver.canMakeMove(Color.BLACK));
+            assertTrue(checkGameOver.canMakeMove(Color.WHITE));
+        }
+    }
+
+    @Test
+    public void testOverMaxMakeMove() {
+        CheckGameOver checkGameOver = new CheckGameOver();
+        Color color = Color.BLACK;
+
+        for (int i = 0; i < STONE_COUNT; i++) {
+            assertTrue(checkGameOver.canMakeMove(color));
+        }
+        assertFalse(checkGameOver.canMakeMove(color));
+    }
+
+    @Test
+    public void testCanSkipTurn() {
+        CheckGameOver checkGameOver = new CheckGameOver();
+        assertTrue(checkGameOver.canSkipTurn());
+        assertFalse(checkGameOver.canSkipTurn());
+    }
+
+    @Test
+    public void testResetSkipCount() {
+        CheckGameOver checkGameOver = new CheckGameOver();
+        assertTrue(checkGameOver.canSkipTurn());
+        checkGameOver.resetSkipCount();
+        assertTrue(checkGameOver.canSkipTurn());
+    }
+}
