2fa6f44f609bbafb9bd487aafed532639aa1e468
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-08-04 00:29:14 +0600

    feat: добавил класс, проверяющий состояние игры на возможность завершения

---
+++ modules/game/src/main/java/io/deeplay/intership/game/CheckGameOver.java
@@ -0,0 +1,63 @@
+package io.deeplay.intership.game;
+
+/**
+ * Класс {@code  CheckGameOver} содержит методы проверки окончания игры.
+ * Он отслеживает количество черных камней, белых камней и статус пропуска ходов.
+ */
+public class CheckGameOver {
+    private static final int DEFAULT_STONE_COUNT = 160;
+    private boolean hasOneSkip;
+    private int blackStonesCount;
+    private int whiteStonesCount;
+
+    public CheckGameOver() {
+        hasOneSkip = false;
+        this.blackStonesCount = DEFAULT_STONE_COUNT;
+        this.whiteStonesCount = DEFAULT_STONE_COUNT;
+    }
+
+    /**
+     * Проверяет, можно ли сделать ход указанным цветом. Уменьшает количество камней указанного цвета.
+     *
+     * @param color цвет игрока, делающего ход
+     * @return {@code true}, если ход можно сделать, иначе {@code false}
+     * @throws {@link IllegalStateException}, если цвет не черный или белый
+     */
+    public boolean canMakeMove(Color color) {
+        return switch (color) {
+            case BLACK -> hasStones(--blackStonesCount);
+            case WHITE -> hasStones(--whiteStonesCount);
+            default -> throw new IllegalArgumentException("Незарегестрированное значение: " + color);
+        };
+    }
+
+    /**
+     * Проверяет, можно ли пропустить ход.
+     *
+     * @return {@code true}, если ход можно пропустить, иначе {@code false}
+     */
+    public boolean canSkipTurn() {
+        if (!hasOneSkip) {
+            hasOneSkip = true;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Сбрасывает счетчик пропусков на {@code false}.
+     */
+    public void resetSkipCount() {
+        hasOneSkip = false;
+    }
+
+    /**
+     * Проверяет, остались ли камни, чтобы походить.
+     *
+     * @param stoneNumber количество камней
+     * @return {@code true}, если остались камни, иначе {@code false}
+     */
+    private boolean hasStones(int stoneNumber) {
+        return stoneNumber >= 0;
+    }
+}
