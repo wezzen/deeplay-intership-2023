63378a872a0a57d2a1ef868d8a9a17aaacf50396
Simon170522 <safonof.com@gmail.com>
2023-08-04 12:48:36 +0700

    -

--- modules/server/src/main/java/io/deeplay/intership/server/Server.java
+++ modules/server/src/main/java/io/deeplay/intership/server/Server.java
@@ -2,9 +2,6 @@ package io.deeplay.intership.server;
 
 public class Server {
 
-    public static void main(final String[] args) {
-        System.out.println("Hello, World");
-
-    }
+    public static void main(final String[] args) { System.out.println("Hello, World"); }
 
 }
