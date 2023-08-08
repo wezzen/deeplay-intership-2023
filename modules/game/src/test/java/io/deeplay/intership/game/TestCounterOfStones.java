ccd7aee1e27416cf1697c5f86571171ebecf43e8
Angedon <g.peretolchin@g.nsu.ru>
2023-08-08 16:38:14 +0400

    fix убрал статик, убрал ненужный конструктор

--- modules/game/src/test/java/io/deeplay/intership/game/TestCounterOfStones.java
+++ modules/game/src/test/java/io/deeplay/intership/game/TestCounterOfStones.java
@@ -4,12 +4,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class TestCounterOfStones {
+    public void setField(String s, Board board){
+        Stone[][] field = board.getField();
+        int sizeOfField = field.length;
+        int strSize = s.length();
+        for(int i = 0; i < strSize; i++){
+            if(s.charAt(i) == 'W'){
+                field[i / sizeOfField][i % sizeOfField].setColor(Color.WHITE);
+            }
+            else if(s.charAt(i) == 'B'){
+                field[i / sizeOfField][i % sizeOfField].setColor(Color.BLACK);
+            }
+            else{
+                field[i / sizeOfField][i % sizeOfField].setColor(Color.EMPTY);
+            }
+        }
+    }
     @Test
     public void testCountCapturedEmptyStones(){
-        String s = "EEBEEEEEWEEBEEEEEWEEBBEEWWEEWEEEEWEEWEEWEEEEWEWEEEBBEEEWEEEBEBEEEEEEBEEEBEEEEBEEEEEEEEEEEEEEEEEEEEEE";
-        CounterOfStones counterOfStones = new CounterOfStones(s, 10);
+        String s = "EEBEEEEWEEBEEEEWEEBBEEWWEWEEEEWEEWEWEEEEWEWEEBBEEEWEEBEBEEEEEBEEBBEEEBEEEBEEEEEEE";
+        Board board = new Board();
+        setField(s, board);
+        CounterOfStones counterOfStones = new CounterOfStones(board);
         counterOfStones.countCapturedEmptyStones();
-        assertEquals(3, counterOfStones.getBlackPoints());
-        assertEquals(7, counterOfStones.getWhitePoints());
+        assertEquals(6, counterOfStones.getBlackPoints());
+        assertEquals(8, counterOfStones.getWhitePoints());
     }
 }
