2709fc50b88d6a128a74f28ba4a46da8d95572f8
Angedon <g.peretolchin@g.nsu.ru>
2023-08-10 17:09:34 +0400

    fix добавил возвращение свободных камней при удалении группы

--- modules/game/src/test/java/io/deeplay/intership/game/CounterOfStonesTest.java
+++ modules/game/src/test/java/io/deeplay/intership/game/CounterOfStonesTest.java
@@ -3,7 +3,7 @@ package io.deeplay.intership.game;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-public class TestCounterOfStones {
+public class CounterOfStonesTest {
     public void setField(String s, Board board){
         Stone[][] field = board.getField();
         int sizeOfField = field.length;
@@ -27,6 +27,7 @@ public class TestCounterOfStones {
         Board board = new Board();
         setField(s, board);
         CounterOfStones counterOfStones = new CounterOfStones(board);
+        counterOfStones.print();
         counterOfStones.countCapturedEmptyStones();
         assertEquals(6, counterOfStones.getBlackPoints());
         assertEquals(8, counterOfStones.getWhitePoints());
