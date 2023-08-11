e2113252d25a00a17a815d83b6a78952c5f01ea2
Simon170522 <safonof.com@gmail.com>
2023-08-11 18:55:58 +0700

    new сделал бота

--- modules/bot/src/main/java/io/deeplay/intership/bot/Bot.java
+++ modules/bot/src/main/java/io/deeplay/intership/bot/Bot.java
@@ -11,8 +11,8 @@ public class Bot {
     public Stone randomMove(Board board){
         List<Stone> emptyStones = new ArrayList<>();
         Stone[][] field = board.getField();
-        for (int i = 0; i < board.getDefaultBoardSize(); i++) {
-            for (int j = 0; j < board.getDefaultBoardSize(); j++) {
+        for (int i = 0; i < board.DEFAULT_BOARD_SIZE; i++) {
+            for (int j = 0; j < board.DEFAULT_BOARD_SIZE; j++) {
                 Stone stone = field[i][j];
                 if(stone.getColor() == Color.EMPTY){
                     emptyStones.add(stone);
