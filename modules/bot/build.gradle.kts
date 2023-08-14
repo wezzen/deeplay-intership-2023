0c7b5615eab94c37a8b8e1e18f9bb189b56ed82c
Simon170522 <safonof.com@gmail.com>
2023-08-14 15:08:43 +0700

    fix переделал бота

--- modules/bot/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("java")
-    id("application")
-}
-
-dependencies {
-    implementation(project(mapOf("path" to ":game")))
-    implementation(project(mapOf("path" to ":terminal-ui")))
-}
