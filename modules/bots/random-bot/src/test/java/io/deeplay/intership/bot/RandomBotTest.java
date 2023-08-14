db14035296af6b17c5224648df042cb6645754d2
Simon170522 <safonof.com@gmail.com>
2023-08-14 15:49:18 +0700

    fix разрешил конфликты

--- modules/bots/random-bot/src/test/java/io/deeplay/intership/bot/RandomBotTest.java
+++ modules/bots/random-bot/src/test/java/io/deeplay/intership/bot/RandomBotTest.java
@@ -4,6 +4,7 @@ import io.deeplay.intership.model.Board;
 import io.deeplay.intership.model.Color;
 import io.deeplay.intership.model.Stone;
 
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
