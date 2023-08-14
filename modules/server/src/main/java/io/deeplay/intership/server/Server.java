cf66202d568579280eee574e1a49defa5f4b2363
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-08-14 19:09:13 +0600

    feat: переделал вызов main из класса Server под сокеты и работу в многопоточности с использованием ExecutorService.

--- modules/server/src/main/java/io/deeplay/intership/server/Server.java
+++ modules/server/src/main/java/io/deeplay/intership/server/Server.java
@@ -1,8 +1,31 @@
 package io.deeplay.intership.server;
 
-public class Server {
-    public static void main(final String[] args) {
+import org.apache.log4j.Logger;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private static final Logger logger = Logger.getLogger(Server.class);
+    private static final int PORT = 5000;
+
+    public static void main(final String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        logger.info("Server was started...");
+        logger.info("Port number " + PORT);
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                executorService.execute(new ClientHandler(clientSocket));
+            }
+        } catch (IOException ex) {
+            logger.debug(ex.getMessage());
+        }
+        executorService.shutdown();
     }
 
 }
