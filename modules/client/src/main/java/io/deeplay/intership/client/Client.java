1f0302c8b430ed91e378238912c8fc7e9e03bfdf
Simon170522 <safonof.com@gmail.com>
2023-08-15 15:40:09 +0700

    fix Добавил сокеты

--- modules/client/src/main/java/io/deeplay/intership/client/Client.java
+++ modules/client/src/main/java/io/deeplay/intership/client/Client.java
@@ -1,9 +1,28 @@
 package io.deeplay.intership.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
 
     public static void main(final String[] args) {
-        System.out.println("Hello, World");
+
+        try {
+            Socket socket = new Socket("localhost",8000);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+            //
+            // Код клиента - обработка
+            //
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
