24fb8dd4c2abbdd9075dfc44c3da3b97e8d6550b
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-08-15 19:36:09 +0600

    fix: оставил заглушки в методах сервисных классов

--- modules/service/src/main/java/io/deeplay/intership/service/UserService.java
+++ modules/service/src/main/java/io/deeplay/intership/service/UserService.java
@@ -1,35 +1,39 @@
 package io.deeplay.intership.service;
 
 
+import io.deeplay.intership.dto.request.LoginDtoRequest;
+import io.deeplay.intership.dto.request.LogoutDtoRequest;
+import io.deeplay.intership.dto.request.RegistrationDtoRequest;
+import io.deeplay.intership.dto.response.InfoDtoResponse;
+import io.deeplay.intership.dto.response.LoginDtoResponse;
 import io.deeplay.intership.model.User;
 
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class UserService {
-    private final ConcurrentMap<String, User> users;
+    private final ConcurrentMap<String, User> authorizedUsers;
+
 
     public UserService() {
-        this.users = new ConcurrentHashMap<>();
+        this.authorizedUsers = new ConcurrentHashMap<>();
     }
 
     public boolean isAuthorized(final String token) {
-        return users.containsKey(token);
+        return authorizedUsers.containsKey(token);
     }
 
-    public String authorization(final String request) {
-
-        String token = UUID.randomUUID().toString();
-//        users.put(token, user);
-        return token;
-    }
-
-    public String register(final String request) {
+    public LoginDtoResponse authorization(final LoginDtoRequest dtoRequest) {
+        //FIXME: заменить заглушку на работающий код
         return null;
     }
 
-    public String logout(final String request) {
+    public InfoDtoResponse register(final RegistrationDtoRequest dtoRequest) {
+
+        return null;
+    }
+
+    public InfoDtoResponse logout(final LogoutDtoRequest dtoRequest) {
         return null;
     }
 }
