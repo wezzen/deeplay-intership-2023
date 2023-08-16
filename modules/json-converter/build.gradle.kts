8781d319e97d1811f61ca3f45784a1776244afdd
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-08-16 17:56:02 +0600

    fix

--- modules/json-converter/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-dependencies {
-    implementation("com.fasterxml.jackson.core:jackson-core:2.15.2")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.2")
-    implementation("org.projectlombok:lombok:1.18.22")
-}
