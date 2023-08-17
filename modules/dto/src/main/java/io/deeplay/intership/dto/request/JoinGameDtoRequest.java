b7fbc4bcb3b4886349b3d8bec81d5fd2355674e6
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-08-17 21:43:05 +0600

    fix: изменены классы, чтобы была возможность выбирать цвет при подключению к игре

--- modules/dto/src/main/java/io/deeplay/intership/dto/request/JoinGameDtoRequest.java
+++ modules/dto/src/main/java/io/deeplay/intership/dto/request/JoinGameDtoRequest.java
@@ -2,6 +2,6 @@ package io.deeplay.intership.dto.request;
 
 import io.deeplay.intership.dto.RequestType;
 
-public record JoinGameDtoRequest(RequestType requestType, String gameId, String token) {
+public record JoinGameDtoRequest(RequestType requestType, String gameId, String token, String color) {
 
 }
