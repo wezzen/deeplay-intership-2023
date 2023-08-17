a9f8d3f941a5eab8612042722c0fa7fa808e20de
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-08-17 14:35:05 +0600

    feat: добавлены enum с статусом ответа и сообщением

--- modules/server/build.gradle.kts
+++ modules/server/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     implementation(project(":dto"))
     implementation(project(":model"))
     implementation(project(":service"))
+    implementation(project(":server-exception"))
     implementation("com.fasterxml.jackson.core:jackson-core:2.15.2")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.2")
