5411b8a6f03bee53ab2777467f6735f42dabc0a7
Angedon <g.peretolchin@g.nsu.ru>
2023-08-17 13:41:33 +0400

    Перенес JsonConverter в другой модуль

--- modules/json-converter/build.gradle.kts
+++ modules/json-converter/build.gradle.kts
@@ -2,7 +2,6 @@ dependencies {
     implementation(project(":dto"))
     implementation(project(":model"))
     implementation(project(":service"))
-    implementation(project(":json-converter"))
     implementation("com.fasterxml.jackson.core:jackson-core:2.15.2")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
 }
--- modules/json-converter/src/test/java/io/deeplay/intership/json/JSONConverterTest.java
+++ modules/json-converter/src/test/java/io/deeplay/intership/json/JSONConverterTest.java
@@ -1,4 +1,4 @@
-package io.deeplay.intership.server;
+package io.deeplay.intership.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
