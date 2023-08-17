b9bbfb106bb3f8c0fa6d7aee67865aee91a4e63b
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-08-17 10:26:55 +0600

    feat: добавил поле с сообщение об ошибке

--- modules/server-exception/src/main/java/io/deeplay/intership/exception/ServerException.java
+++ modules/server-exception/src/main/java/io/deeplay/intership/exception/ServerException.java
@@ -2,8 +2,10 @@ package io.deeplay.intership.exception;
 
 public class ServerException extends Exception {
     public final ErrorCode errorCode;
+    public final String message;
 
     public ServerException(ErrorCode errorCode) {
         this.errorCode = errorCode;
+        this.message = errorCode.message;
     }
 }
