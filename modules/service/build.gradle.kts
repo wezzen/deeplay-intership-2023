d91c1dc59156f99eab943940593725233088db9e
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-08-17 14:35:05 +0600

    feat: добавлены enum с статусом ответа и сообщением

--- modules/service/build.gradle.kts
+++ modules/service/build.gradle.kts
@@ -2,4 +2,6 @@ dependencies {
     implementation(project(":dto"))
     implementation(project(":model"))
     implementation(project(":game"))
+    implementation(project(":dto-validator"))
+    implementation(project(":server-exception"))
 }
