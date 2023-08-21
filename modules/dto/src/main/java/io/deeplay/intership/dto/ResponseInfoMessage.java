9729dbdd56d5ab33e5b969c80847d5dba0f0ac58
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-08-21 16:10:07 +0600

    fix: поменял валидацию для dto-классов

--- modules/dto/src/main/java/io/deeplay/intership/dto/ResponseInfoMessage.java
+++ modules/dto/src/main/java/io/deeplay/intership/dto/ResponseInfoMessage.java
@@ -5,7 +5,9 @@ public enum ResponseInfoMessage {
     SUCCESS_AUTHORIZATION("You entered!"),
     SUCCESS_LOGOUT("You've log out!"),
     SUCCESS_CREATE_GAME("Have a good game!"),
-    SUCCESS_JOIN_GAME("Have a good game!")
+    SUCCESS_JOIN_GAME("Have a good game!"),
+    SUCCESS_TURN("You can move!"),
+
     ;
 
     public final String message;
