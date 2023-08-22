2877a6bf698b6e0a067a3b978d7e28a4605e8774
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-08-22 23:06:09 +0600

    test: добавлен тест для User

--- modules/model/src/test/java/io/deeplay/intership/model/UserTest.java
+++ modules/model/src/test/java/io/deeplay/intership/model/UserTest.java
@@ -1,4 +1,39 @@
 package io.deeplay.intership.model;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UserTest {
+    @Test
+    public void testConstructor() {
+        final String login = UUID.randomUUID().toString();
+        final String password = UUID.randomUUID().toString();
+        final User user = new User(login, password);
+        assertAll(
+                () -> assertNotNull(user.login()),
+                () -> assertEquals(login, user.login()),
+                () -> assertNotNull(user.passwordHash()),
+                () -> assertEquals(password, user.passwordHash()),
+                () -> assertNull(user.token())
+        );
+    }
+
+    @Test
+    public void testFullConstructor() {
+        final String login = UUID.randomUUID().toString();
+        final String password = UUID.randomUUID().toString();
+        final String token = UUID.randomUUID().toString();
+        final User user = new User(login, password, token);
+        assertAll(
+                () -> assertNotNull(user.login()),
+                () -> assertEquals(login, user.login()),
+                () -> assertNotNull(user.passwordHash()),
+                () -> assertEquals(password, user.passwordHash()),
+                () -> assertNotNull(user.token()),
+                () -> assertEquals(token, user.token())
+        );
+    }
 }
