c47fb22eb18db5aaacbbc70c6e1856c33b35707d
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-08-22 11:29:54 +0600

    feat: реализованы методы turn и pass в GameSession; модифицированы тесты, чтобы проверять работу сервисов и GameSession

--- modules/game/src/main/java/io/deeplay/intership/game/Game.java
+++ modules/game/src/main/java/io/deeplay/intership/game/Game.java
@@ -34,14 +34,6 @@ public class Game {
         return board;
     }
 
-    public Stone[][] analyzeMove(Stone stone) {
-        if (stone.getColor() == Color.EMPTY) {
-            return skipTurn(stone.getColor());
-        } else {
-            return makeMove(stone);
-        }
-    }
-
     public Stone[][] skipTurn(Color color) {
         if (!checkGameOver.canSkipTurn()) {
             endGame();
--- modules/game/src/main/java/io/deeplay/intership/game/GameSession.java
+++ modules/game/src/main/java/io/deeplay/intership/game/GameSession.java
@@ -9,20 +9,23 @@ import io.deeplay.intership.model.Stone;
 public class GameSession {
     private final String gameId;
     private final Game game;
+    private boolean isStarted;
     private Player creator;
     private Player blackPlayer;
     private Player whitePlayer;
+    private Player currentTurn;
 
     public GameSession(final String gameId) {
         this.gameId = gameId;
         this.game = new Game();
+        this.isStarted = false;
     }
 
     public String getGameId() {
         return gameId;
     }
 
-    public synchronized void addCreator(final Player player) {
+    public void addCreator(final Player player) {
         creator = player;
     }
 
@@ -40,6 +43,20 @@ public class GameSession {
         } else {
             whitePlayer = creator;
         }
+        currentTurn = blackPlayer;
+        isStarted = true;
+    }
+
+    public synchronized Stone[][] turn(final Player player, final Stone stone) throws ServerException {
+        isNotStarted();
+        checkTurnOrder(player);
+        return game.makeMove(stone);
+    }
+
+    public synchronized Stone[][] pass(final Player player) throws ServerException {
+        isNotStarted();
+        checkTurnOrder(player);
+        return game.skipTurn(Color.valueOf(player.color()));
     }
 
     private void checkEnemyColor(final Player player) throws ServerException {
@@ -54,6 +71,18 @@ public class GameSession {
             blackPlayer = player;
         } else {
             whitePlayer = player;
+        }
+    }
+
+    private synchronized void isNotStarted() throws ServerException {
+        if (!isStarted) {
+            throw new ServerException(ErrorCode.GAME_NOT_STARTED);
+        }
+    }
+
+    private synchronized void checkTurnOrder(final Player player) throws ServerException {
+        if (!currentTurn.login().equals(player.login())) {
+            throw new ServerException(ErrorCode.INVALID_TURN_ORDER);
         }
     }
 }
