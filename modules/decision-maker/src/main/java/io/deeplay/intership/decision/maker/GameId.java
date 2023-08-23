e419445713d20842eeef0ba370d98197f276b16a
Simon170522 <safonof.com@gmail.com>
2023-08-23 16:51:29 +0700

    new добавил ui интерфейс и подставил реализацию

--- modules/decision-maker/src/main/java/io/deeplay/intership/decision/maker/GameId.java
+++ modules/decision-maker/src/main/java/io/deeplay/intership/decision/maker/GameId.java
@@ -1,6 +1,7 @@
 package io.deeplay.intership.decision.maker;
 
 import io.deeplay.intership.dto.RequestType;
+import io.deeplay.intership.model.Color;
 
-public record GameId(RequestType type, int gameId) {
+public record GameId(RequestType type, boolean withBot, Color color, int size, int gameId) {
 }
