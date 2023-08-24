7402e7d6a8adeb23c4b2ada395b7bce555aea32d
Simon170522 <safonof.com@gmail.com>
2023-08-24 19:26:57 +0700

    fix поправил тесты

--- modules/client/src/test/java/io/deeplay/intership/client/ClientTest.java
+++ modules/client/src/test/java/io/deeplay/intership/client/ClientTest.java
@@ -9,14 +9,13 @@ import java.net.ConnectException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClientTest {
-
     @Test
     void creatingInstanceTest() {
         assertDoesNotThrow(Client::new);
+        assertDoesNotThrow(Client::init);
+        assertDoesNotThrow(Client::joinGame);
+        assertDoesNotThrow(Client::sendRequest);
+        assertDoesNotThrow(Client::makeMove);
+        assertDoesNotThrow(Client::skipTurn);
     }
-    @Test
-    void clientProcessTest() throws IOException {
-    }
-
-
 }
