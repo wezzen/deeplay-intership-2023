11811fd23ff1b5505f5d32bdd2ac201024eca8e6
Simon170522 <safonof.com@gmail.com>
2023-08-27 00:18:44 +0700

    fix поправил тесты. Убрал лишнее в клиенте. Поправил pr.

--- modules/client/build.gradle.kts
+++ modules/client/build.gradle.kts
@@ -4,4 +4,5 @@ dependencies {
     implementation(project(":decision-maker"))
     implementation(project(":terminal-ui"))
     implementation(project(":json-converter"))
+    implementation(project(":user-interface"))
 }
