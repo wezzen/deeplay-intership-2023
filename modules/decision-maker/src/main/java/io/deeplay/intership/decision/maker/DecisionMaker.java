b1fbba0e0b3aa98fedcc0b33e62fc36a633f81fa
Angedon <g.peretolchin@g.nsu.ru>
2023-08-27 11:32:23 +0400

    fix conflicts

--- modules/decision-maker/src/main/java/io/deeplay/intership/decision/maker/DecisionMaker.java
+++ modules/decision-maker/src/main/java/io/deeplay/intership/decision/maker/DecisionMaker.java
@@ -2,7 +2,6 @@ package io.deeplay.intership.decision.maker;
 
 import io.deeplay.intership.model.Color;
 
-<<<<<<< HEAD
 import java.io.IOException;
 
 public interface DecisionMaker {
@@ -13,11 +12,4 @@ public interface DecisionMaker {
     public GameId getGameId() throws IOException;
 
     public Color getColor() throws IOException;
-=======
-public interface DecisionMaker {
-    public LoginPassword getLoginPassword();
-    public GameAction getGameAction();
-    public GameId getGameId();
-    public Color getColor();
->>>>>>> 326b04d (эскиз gui)
 }
--- modules/decision-maker/src/main/java/io/deeplay/intership/decision/maker/LoginPassword.java
+++ modules/decision-maker/src/main/java/io/deeplay/intership/decision/maker/LoginPassword.java
@@ -1,9 +1,6 @@
 package io.deeplay.intership.decision.maker;
 
 import io.deeplay.intership.dto.RequestType;
-<<<<<<< HEAD
-=======
 
->>>>>>> 326b04d (эскиз gui)
 public record LoginPassword(RequestType type, String login, String password) {
 }
