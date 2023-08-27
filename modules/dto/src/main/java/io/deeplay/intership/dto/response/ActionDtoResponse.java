c384e2bd91eb2587c9bf4965f3d8b7485fe56301
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-08-27 15:33:18 +0600

    fix: модифицированы response-классы; все response наследуются от базового класса

--- modules/dto/src/main/java/io/deeplay/intership/dto/response/ActionDtoResponse.java
+++ modules/dto/src/main/java/io/deeplay/intership/dto/response/ActionDtoResponse.java
@@ -2,5 +2,19 @@ package io.deeplay.intership.dto.response;
 
 import io.deeplay.intership.model.Stone;
 
-public record ActionDtoResponse(String message, String status, Stone[][] gameField) {
+public class ActionDtoResponse extends BaseDtoResponse {
+    public final String status;
+    public final String message;
+    public final Stone[][] gameField;
+
+    public ActionDtoResponse(ResponseType responseType, String status, String message, Stone[][] gameField) {
+        super(responseType);
+        this.status = status;
+        this.message = message;
+        this.gameField = gameField;
+    }
+
+    public ActionDtoResponse(String status, String message, Stone[][] gameField) {
+        this(ResponseType.ACTION, status, message, gameField);
+    }
 }
---
+++ modules/dto/src/main/java/io/deeplay/intership/dto/response/BaseDtoResponse.java
@@ -0,0 +1,4 @@
+package io.deeplay.intership.dto.response;
+
+public record BaseDtoResponse(ResponseType responseType) {
+}
