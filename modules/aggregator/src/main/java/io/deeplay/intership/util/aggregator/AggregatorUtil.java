e4f03223fec51764b620a638dd97d182d6d8e5c8
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-10-03 16:00:44 +0600

    docs: добавлена документация для модуля aggregator

--- modules/aggregator/src/main/java/io/deeplay/intership/util/aggregator/AggregatorUtil.java
+++ modules/aggregator/src/main/java/io/deeplay/intership/util/aggregator/AggregatorUtil.java
@@ -7,33 +7,75 @@ import io.deeplay.intership.model.User;
 
 import java.util.Optional;
 
+/**
+ * Класс {@code AggregatorUtil} предоставляет функционал для работы с коллекциями данных представленный
+ * {@link DataCollectionsAggregator}. Он позволяет добавлять и получать информацию об играх, пользователях и их
+ * токенах.
+ */
 public class AggregatorUtil {
     private final DataCollectionsAggregator collectionsAggregator;
 
-    public AggregatorUtil(DataCollectionsAggregator collectionsAggregator) {
+    public AggregatorUtil(final DataCollectionsAggregator collectionsAggregator) {
         this.collectionsAggregator = collectionsAggregator;
     }
 
+    /**
+     * Добавляет нового пользователя в коллекцию.
+     *
+     * @param user Объект {@link User}, который нужно добавить.
+     */
     public void addNewUser(final User user) {
         collectionsAggregator.users().put(user.login(), user);
     }
 
+    /**
+     * Добавляет связь между токеном и пользователем в коллекцию авторизованных пользователей.
+     *
+     * @param token Токен пользователя.
+     * @param user  Объект {@link User}, связанный с токеном.
+     */
     public void addUsersToken(final String token, final User user) {
         collectionsAggregator.tokenToUser().put(token, user);
     }
 
+    /**
+     * Добавляет связь между пользователем и игрой в коллекцию.
+     *
+     * @param token  Токен пользователя.
+     * @param gameId Идентификатор игры.
+     */
     public void addUserToGame(final String token, final String gameId) {
         collectionsAggregator.playerToGame().put(token, gameId);
     }
 
+    /**
+     * Добавляет игровую сессию в коллекцию.
+     *
+     * @param gameId      Идентификатор игры.
+     * @param gameSession Объект {@link GameSession}, представляющий игровую сессию.
+     */
     public void addGameSession(final String gameId, final GameSession gameSession) {
         collectionsAggregator.idToGameSession().put(gameId, gameSession);
     }
 
+    /**
+     * Получает пользователя по его логину.
+     *
+     * @param login Логин пользователя.
+     * @return Объект {@link User}, если пользователь найден; в противном случае {@code Optional.empty()}.
+     * @throws ServerException Если пользователь не найден, генерируется исключение {@code ServerException}.
+     */
     public Optional<User> getUserByLogin(final String login) throws ServerException {
         return Optional.ofNullable(collectionsAggregator.users().get(login));
     }
 
+    /**
+     * Получает пользователя по его токену.
+     *
+     * @param token Токен пользователя.
+     * @return Объект {@link User}, если пользователь найден.
+     * @throws ServerException Если пользователь не найден, генерируется исключение {@code ServerException}.
+     */
     public User getUserByToken(final String token) throws ServerException {
         User user = collectionsAggregator.tokenToUser().get(token);
         if (user == null) {
@@ -42,7 +84,13 @@ public class AggregatorUtil {
         return user;
     }
 
-
+    /**
+     * Получает игровую сессию по ее идентификатору.
+     *
+     * @param gameId Идентификатор игры.
+     * @return Объект {@link GameSession}, представляющий игровую сессию.
+     * @throws ServerException Если игровая сессия не найдена, генерируется исключение {@code ServerException}.
+     */
     public GameSession getGameSessionById(final String gameId) throws ServerException {
         GameSession gameSession = collectionsAggregator.idToGameSession().get(gameId);
         if (gameSession == null) {
@@ -51,6 +99,13 @@ public class AggregatorUtil {
         return gameSession;
     }
 
+    /**
+     * Получает игровую сессию по токену пользователя.
+     *
+     * @param token Токен пользователя.
+     * @return Объект {@link GameSession}, представляющий игровую сессию.
+     * @throws ServerException Если игровая сессия не найдена, генерируется исключение {@code ServerException}.
+     */
     public GameSession getGameByUserToken(final String token) throws ServerException {
         final String gameId = collectionsAggregator.playerToGame().get(token);
         if (gameId == null) {
@@ -59,6 +114,11 @@ public class AggregatorUtil {
         return getGameSessionById(gameId);
     }
 
+    /**
+     * Удаляет связь между токеном и пользователем из коллекции с авторизованными пользователями.
+     *
+     * @param token Токен пользователя.
+     */
     public void removeUsersToken(final String token) {
         collectionsAggregator.tokenToUser().remove(token);
     }
--- modules/aggregator/src/main/java/io/deeplay/intership/util/aggregator/DataCollectionsAggregator.java
+++ modules/aggregator/src/main/java/io/deeplay/intership/util/aggregator/DataCollectionsAggregator.java
@@ -10,6 +10,15 @@ import org.apache.log4j.Logger;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+/**
+ * Класс {@code DataCollectionsAggregator} представляет собой агрегатор коллекций данных,
+ * используемых в системе. Этот класс содержит четыре потокобезопасных коллекции данных:
+ * - {@code idToGameSession} для хранения игровых сессий с их идентификаторами.
+ * - {@code tokenToUser} для хранения связей между токенами и пользователями.
+ * - {@code playerToGame} для хранения связей между пользователями и играми.
+ * - {@code users} для хранения пользователей по их логинам.
+ * Класс также выполняет инициализацию коллекции {@code users} данными из базы данных, используя объект {@link UserDao}.
+ */
 public record DataCollectionsAggregator(
         ConcurrentMap<String, GameSession> idToGameSession,
         ConcurrentMap<String, User> tokenToUser,
@@ -18,11 +27,19 @@ public record DataCollectionsAggregator(
     private static final Logger logger = Logger.getLogger(DataCollectionsAggregator.class);
     private static final UserDao userDao = new UserDaoImpl();
 
+    /**
+     * Конструктор класса {@code DataCollectionsAggregator} для инициализации коллекций данных.
+     *
+     * @param idToGameSession Коллекция для игровых сессий.
+     * @param tokenToUser     Коллекция для хранения авторизованных пользователей.
+     * @param playerToGame    Коллекция для связей пользователей и игр.
+     * @param users           Коллекция для хранения пользователей.
+     */
     public DataCollectionsAggregator(
-            ConcurrentMap<String, GameSession> idToGameSession,
-            ConcurrentMap<String, User> tokenToUser,
-            ConcurrentMap<String, String> playerToGame,
-            ConcurrentMap<String, User> users) {
+            final ConcurrentMap<String, GameSession> idToGameSession,
+            final ConcurrentMap<String, User> tokenToUser,
+            final ConcurrentMap<String, String> playerToGame,
+            final ConcurrentMap<String, User> users) {
         this.idToGameSession = idToGameSession;
         this.tokenToUser = tokenToUser;
         this.playerToGame = playerToGame;
@@ -36,6 +53,9 @@ public record DataCollectionsAggregator(
         }
     }
 
+    /**
+     * Конструктор класса {@code DataCollectionsAggregator} без параметров для создания пустых коллекций данных.
+     */
     public DataCollectionsAggregator() {
         this(new ConcurrentHashMap<>(), new ConcurrentHashMap<>(), new ConcurrentHashMap<>(), new ConcurrentHashMap<>());
     }
