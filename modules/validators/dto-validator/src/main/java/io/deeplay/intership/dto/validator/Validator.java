a0520d2f32b714d7bb15c34550406008e6395161
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-10-03 13:45:13 +0600

    docs: добавлена документация для модуля dto-validator; добавлен модификатор final аргументам методов

--- modules/validators/dto-validator/src/main/java/io/deeplay/intership/dto/validator/Validator.java
+++ modules/validators/dto-validator/src/main/java/io/deeplay/intership/dto/validator/Validator.java
@@ -1,6 +1,5 @@
 package io.deeplay.intership.dto.validator;
 
-import io.deeplay.intership.dto.request.*;
 import io.deeplay.intership.dto.request.authorization.LoginDtoRequest;
 import io.deeplay.intership.dto.request.authorization.LogoutDtoRequest;
 import io.deeplay.intership.dto.request.authorization.RegistrationDtoRequest;
@@ -15,44 +14,91 @@ import io.deeplay.intership.model.Color;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Класс {@link Validator} предоставляет методы для проверки данных, передаваемых в различных
+ * запросах, и генерации исключений типа {@link ServerException} в случае неверных данных.
+ */
 public class Validator {
+    // Массив допустимых размеров поля
     private final int[] FIELD_SIZES = {9, 11, 13, 15, 17, 19};
 
-    public void validationCreateGameDto(CreateGameDtoRequest dtoRequest) throws ServerException {
+    /**
+     * Проверяет данные из объекта {@link CreateGameDtoRequest}.
+     *
+     * @param dtoRequest Объект {@link CreateGameDtoRequest} для проверки.
+     * @throws ServerException Если данные недействительны, генерируется исключение {@link ServerException}.
+     */
+    public void validationCreateGameDto(final CreateGameDtoRequest dtoRequest) throws ServerException {
         isExistColor(dtoRequest.color);
         isValidToken(dtoRequest.token);
         isValidSize(dtoRequest.size);
     }
 
-    public void validationFinishGameDto(FinishGameDtoRequest dtoRequest) throws ServerException {
+    /**
+     * Проверяет данные из объекта {@link FinishGameDtoRequest}.
+     *
+     * @param dtoRequest Объект {@link FinishGameDtoRequest} для проверки.
+     * @throws ServerException Если данные недействительны, генерируется исключение {@link ServerException}.
+     */
+    public void validationFinishGameDto(final FinishGameDtoRequest dtoRequest) throws ServerException {
         isValidToken(dtoRequest.gameId);
     }
 
-    public void validationJoinGameDto(JoinGameDtoRequest dtoRequest) throws ServerException {
+    /**
+     * Проверяет данные из объекта {@link JoinGameDtoRequest}.
+     *
+     * @param dtoRequest Объект {@link JoinGameDtoRequest} для проверки.
+     * @throws ServerException Если данные недействительны, генерируется исключение {@link ServerException}.
+     */
+    public void validationJoinGameDto(final JoinGameDtoRequest dtoRequest) throws ServerException {
         isValidToken(dtoRequest.gameId);
         isValidToken(dtoRequest.token);
         isExistColor(dtoRequest.color);
     }
 
-    public void validationLoginDto(LoginDtoRequest dtoRequest) throws ServerException {
+    /**
+     * Проверяет данные из объекта {@link LoginDtoRequest}.
+     *
+     * @param dtoRequest Объект {@link LoginDtoRequest} для проверки.
+     * @throws ServerException Если данные недействительны, генерируется исключение {@link ServerException}.
+     */
+    public void validationLoginDto(final LoginDtoRequest dtoRequest) throws ServerException {
         isValidLogin(dtoRequest.login);
         isValidPassword(dtoRequest.passwordHash);
     }
 
-    public void validationLogoutDto(LogoutDtoRequest dtoRequest) throws ServerException {
+    /**
+     * Проверяет данные из объекта {@link LogoutDtoRequest}.
+     *
+     * @param dtoRequest Объект {@link LogoutDtoRequest} для проверки.
+     * @throws ServerException Если данные недействительны, генерируется исключение {@link ServerException}.
+     */
+    public void validationLogoutDto(final LogoutDtoRequest dtoRequest) throws ServerException {
         isValidToken(dtoRequest.token);
     }
 
-    public void validationTurnDto(AnswerDtoRequest dtoRequest) throws ServerException {
+    /**
+     * Проверяет данные из объекта {@link AnswerDtoRequest}.
+     *
+     * @param dtoRequest Объект {@link AnswerDtoRequest} для проверки.
+     * @throws ServerException Если данные недействительны, генерируется исключение {@link ServerException}.
+     */
+    public void validationTurnDto(final AnswerDtoRequest dtoRequest) throws ServerException {
         isValidCoordinates(dtoRequest.row, dtoRequest.column);
     }
 
-    public void validationRegistrationDto(RegistrationDtoRequest dtoRequest) throws ServerException {
+    /**
+     * Проверяет данные из объекта {@link RegistrationDtoRequest}.
+     *
+     * @param dtoRequest Объект {@link RegistrationDtoRequest} для проверки.
+     * @throws ServerException Если данные недействительны, генерируется исключение {@link ServerException}.
+     */
+    public void validationRegistrationDto(final RegistrationDtoRequest dtoRequest) throws ServerException {
         isValidLogin(dtoRequest.login);
         isValidPassword(dtoRequest.passwordHash);
     }
 
-    private void isExistColor(String color) throws ServerException {
+    private void isExistColor(final String color) throws ServerException {
         if (color == null) {
             throw new ServerException(ServerErrorCode.COLOR_DOES_NOT_EXIST);
         }
@@ -63,7 +109,7 @@ public class Validator {
         }
     }
 
-    private void isValidLogin(String login) throws ServerException {
+    private void isValidLogin(final String login) throws ServerException {
         if (login == null ||
                 login.isBlank() ||
                 login.contains(" ")) {
@@ -76,7 +122,7 @@ public class Validator {
         }
     }
 
-    private void isValidToken(String token) throws ServerException {
+    private void isValidToken(final String token) throws ServerException {
         if (token == null ||
                 token.isBlank() ||
                 token.contains(" ")) {
@@ -89,7 +135,7 @@ public class Validator {
         }
     }
 
-    private void isValidSize(int size) throws ServerException {
+    private void isValidSize(final int size) throws ServerException {
         for (var item : FIELD_SIZES) {
             if (item == size) {
                 return;
@@ -105,7 +151,7 @@ public class Validator {
         }
     }
 
-    private void isValidCoordinates(int x, int y) throws ServerException {
+    private void isValidCoordinates(final int x, final int y) throws ServerException {
         if (x > FIELD_SIZES[FIELD_SIZES.length - 1] ||
                 y > FIELD_SIZES[FIELD_SIZES.length - 1] ||
                 x < 0 ||
